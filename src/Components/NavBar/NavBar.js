a61ad221e511c6778578920d26cf4866fea1a76a
Fatima Kammona <fqkammona@gmail.com>
2024-01-29 08:09:20 -0600

    Fixed some navbar formating.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
 
 const NavBar = ({ onGirlCodedClick }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleNavItemClick = () => {
+        setExpanded(false); // Collapse the navbar when an item is clicked
+    };
+
     return (
-        <Navbar bg="light" expand="lg">
-            <Link to="/" className="navbar-brand" onClick={onGirlCodedClick}>GIRL CODED</Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar bg="light" expand="lg" className="custom-navbar" expanded={expanded}>
+            <Link to="/" className="navbar-brand" onClick={() => { onGirlCodedClick(); handleNavItemClick(); }}>GIRL CODED</Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded => !expanded)} />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    {/* Use Link instead of Nav.Link for SPA navigation */}
-                    <Link to="/our-story" className="nav-link">Our Story</Link>
-                    <Link to="/frequently-asked-questions" className="nav-link">FAQ</Link>
-                    <Link to="/meet-the-team" className="nav-link">Meet the Team</Link>
-                    <Button variant="primary">Sign In</Button>
+                <Nav className="mx-auto">
+                    <Link to="/our-story" className="nav-link" onClick={handleNavItemClick}>Our Story</Link>
+                    <Link to="/frequently-asked-questions" className="nav-link" onClick={handleNavItemClick}>FAQ</Link>
+                    <Link to="/meet-the-team" className="nav-link" onClick={handleNavItemClick}>Meet The Team</Link>
                 </Nav>
+                <Button variant="primary" className="ml-auto" onClick={handleNavItemClick}>Sign In</Button>
             </Navbar.Collapse>
         </Navbar>
     );
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="homepage-text">
       <h1>Welcome to our website!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a convallis faucibus, ipsum nisi elementum nisi, vel fringilla nunc nisl et lacus.</p>
+      <p>HOme</p>
     </div>
   );
 };
