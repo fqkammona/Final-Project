d18bee9e04e97390cf1dd90476bb89b24f3fff53
Fatima Kammona <fqkammona@gmail.com>
2024-03-18 18:21:40 -0500

    Saving Home page transition.

--- src/Pages/Home/Mountains.js
+++ src/Pages/Home/Mountains.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Mountains.css';
 
 const Mountains = () => {
+  // Array of rays, adjust the number of rays as needed
+  const rays = Array.from({ length: 12 }, (_, index) => (
+    <div key={index} className="ray" />
+  ));
+
   return (
     <div className="mountain-container">
       <div className="mountain-sky"></div>
       <div className="big-cloud"></div>
-
-      
+      <div className="mountain"></div>
+      <div className="sun">{rays}</div> {/* Place rays within the sun */}
     </div>
   );
 };
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,27 @@
-// HomePage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
-import Phone from './Home/Phone'; // Make sure to import the Phone component
+import Phone from './Home/Phone';
 import Mountains from './Home/Mountains';
 
 const HomePage = () => {
+  const [showComponent, setShowComponent] = useState('phone');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowComponent('mountains');
+    }, 5000); // 5000ms = 5 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className='home-container'>
-
-      <Mountains />
-
+      <div className={`phone-container ${showComponent === 'phone' ? 'visible' : ''}`}>
+        <Phone />
+        <p className="phone-text">Wherever you go, know you're home is safe.</p>
+      </div>
+      <div className={`component mountains ${showComponent === 'mountains' ? 'visible' : ''}`}>
+        <Mountains />
+      </div>
     </div>
   );
 };
