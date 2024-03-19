13b8ab97a52b97eefcf70c1f64c79cd595d9f2b3
Fatima Kammona <fqkammona@gmail.com>
2024-03-18 22:23:50 -0500

    Adding details to car and mountains.

--- src/Pages/Home/Mountains.js
+++ src/Pages/Home/Mountains.js
@@ -12,6 +12,13 @@ const Mountains = () => {
       <div className="mountain-sky"></div>
       <div className="big-cloud"></div>
       <div className="mountain"></div>
+      <div className="hill1"></div>
+      <div className="hill2"></div>
+      <div className="tree">
+      <div className="tree-part tree-bottom"></div>
+      <div className="tree-part tree-middle"></div>
+      <div className="tree-part tree-top"></div>
+    </div>
       <div className="sun">{rays}</div> {/* Place rays within the sun */}
     </div>
   );
