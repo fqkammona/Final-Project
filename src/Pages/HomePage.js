14f6ee027b53f20fc1a75e1418261181092afe70
Fatima Kammona <fqkammona@gmail.com>
2024-03-25 13:41:13 -0500

    The loading page works but i want better.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './HomePage.css';
-import Phone from './Home/Phone';
 import Mountains from './Home/MountainRanges';
 
 const HomePage = () => {
-  const [showComponent, setShowComponent] = useState('');
-
-  useEffect(() => {
-    // Delay showing the components
-    const phoneTimer = setTimeout(() => {
-      setShowComponent('phone');
-    }, 1000);
-
-    const mountainsTimer = setTimeout(() => {
-      setShowComponent('mountains');
-    }, 6000);
-
-    return () => {
-      clearTimeout(phoneTimer);
-      clearTimeout(mountainsTimer);
-    };
-  }, []);
 
   return (
     <div className='home-container'>
-      <div className={`phone-containerr ${showComponent === 'phone' ? 'visible' : 'hidden'}`}>
-        <Phone />
-      </div>
-      <div className={`component mountains ${showComponent === 'mountains' ? 'visible' : 'hidden'}`}>
+      <div className='show-mountains'>
         <Mountains />
       </div>
     </div>
