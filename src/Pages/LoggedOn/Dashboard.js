bd81a746f2d47be480c37ece1b6975042ada89ca
Fatima Kammona <fqkammona@gmail.com>
2024-04-14 11:42:07 -0500

    Saving dashboard display.

--- src/Pages/LoggedOn/Dashboard.js
+++ src/Pages/LoggedOn/Dashboard.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 import { useAuth } from '../../AuthContext';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import { FaCog } from 'react-icons/fa'; // Import the settings icon
-import { db } from '../../firebase-config'; // Make sure this path is correct
+import { FaCog } from 'react-icons/fa';
+import { BsCalendar4Event } from "react-icons/bs";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { IoHelpOutline } from "react-icons/io5";
+import { db } from '../../firebase-config';
 import { doc, getDoc } from 'firebase/firestore';
 
 const Dashboard = () => {
@@ -23,13 +26,30 @@ const Dashboard = () => {
   }, [currentUser]);
 
   return (
-    <div className="home">
-      <div className='header'>
+    <div className="dashboard-home">
+      <div className='dashboard-header'>
         {currentUser ? <h1>Welcome, {firstName}!</h1> : <h1>Welcome!</h1>}
-        <FaCog onClick={() => navigate('/our-story')} style={{ cursor: 'pointer' }} /> {/* Add the settings icon */}
+        <div className="icon-row">
+          <div className="icon-container" onClick={() => navigate('/live-feed')}>
+            <FaCog className="icon" />
+            <span className="icon-title">Settings</span>
+          </div>
+          <div className="icon-container" onClick={() => navigate('/live-feed')}>
+            <BsCalendar4Event className="icon" />
+            <span className="icon-title">Events</span>
+          </div>
+          <div className="icon-container" onClick={() => navigate('/live-feed')}>
+            <IoPhonePortraitOutline className="icon" />
+            <span className="icon-title">notifications</span>
+          </div>
+          <div className="icon-container" onClick={() => navigate('/live-feed')}>
+            <IoHelpOutline className="icon" />
+            <span className="icon-title">Help</span>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Dashboard;
